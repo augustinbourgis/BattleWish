a68402347ae3ad97ba8ca3364b9c2e67b7947927
Aquemz <augustinbourgis@gmail.com>
2022-05-11 22:55:56 +0200

    WIP Mode Facile

--- BlazorApp/BlazorApp/Controller/Game.cs
+++ BlazorApp/BlazorApp/Controller/Game.cs
@@ -1,4 +1,5 @@
 ﻿using BlazorApp.Controller.Enums;
+using BlazorApp.Controller.Ships;
 
 namespace BlazorApp.Controller
 {
@@ -22,26 +23,65 @@ namespace BlazorApp.Controller
 
         public bool PlayerShoot(int x, int y)
         {
-            Ia.GameBoard.Tiles[Utility.Index(new Tile(x,y), Ia.GameBoard.Tiles)].IsShot = true;
-            IsPlayerTurn = Ia.GameBoard.Tiles[Utility.Index(new Tile(x, y), Ia.GameBoard.Tiles)].IsABoat();
+            Player.FiringBoard.Tiles[Utility.Index(new Tile(x,y), Ia.GameBoard.Tiles)].IsShot = true;
+            IsPlayerTurn = Player.FiringBoard.Tiles[Utility.Index(new Tile(x, y), Ia.GameBoard.Tiles)].IsABoat();
+            Ship target = GetIaShip(new Tile(x, y));
+            if (target != null) target.Hits++;
             if (Ia.HasLost)
             {
                 IsFinished = true;
                 End = DateTime.Now;
             }
-            return IsPlayerTurn;
+            if (!IsPlayerTurn) IaShoot();
+            return true;
         }
 
-        public bool IaShoot(int x, int y)
+        public bool IaShoot()
         {
-            Player.GameBoard.Tiles[Utility.Index(new Tile(x, y), Ia.GameBoard.Tiles)].IsShot = true;
-            IsPlayerTurn = !Player.GameBoard.Tiles[Utility.Index(new Tile(x, y), Ia.GameBoard.Tiles)].IsABoat();
+            int x = Utility.Random(0, Player.GameBoard.Width);
+            int y = Utility.Random(0, Player.GameBoard.Height);
+            Ia.FiringBoard.Tiles[Utility.Index(new Tile(x, y), Ia.GameBoard.Tiles)].IsShot = true;
+            IsPlayerTurn = !Ia.FiringBoard.Tiles[Utility.Index(new Tile(x, y), Ia.GameBoard.Tiles)].IsABoat();
             if (Ia.HasLost || Player.HasLost)
             {
                 IsFinished = true;
                 End = DateTime.Now;
             }
+            if (!IsPlayerTurn)
+            {
+                IsPlayerTurn = IaShoot();
+            }
             return IsPlayerTurn;
+        }
+
+        public Ship GetIaShip(Tile t)
+        {
+            if (Ia.GameBoard.Tiles[Utility.Index(t, Ia.GameBoard.Tiles)].IsABoat())
+            {
+                foreach(Ship iaShip in Ia.Ships)
+                {
+                    if (Utility.Contains(t, iaShip.Tiles))
+                    {
+                        return iaShip;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public Ship GetPlayerShip(Tile t)
+        {
+            if (Player.GameBoard.Tiles[Utility.Index(t, Player.GameBoard.Tiles)].IsABoat())
+            {
+                foreach (Ship playerShip in Player.Ships)
+                {
+                    if (Utility.Contains(t, playerShip.Tiles))
+                    {
+                        return playerShip;
+                    }
+                }
+            }
+            return null;
         }
     }
 }
